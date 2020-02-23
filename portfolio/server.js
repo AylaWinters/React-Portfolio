433b1e301699df36c37af632db5bb917d15112ca
Andre Entrekin <andre.entrekin@gmail.com>
2020-02-23 17:48:57 -0600

    cleaning code

--- portfolio/server.js
+++ portfolio/server.js
@@ -1,16 +1,19 @@
-var express = require('express');
-var compression = require('compression')
+var express = require("express");
+var compression = require("compression");
 var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(compression())
-app.use(express.static("public"));
+app.use(compression());
 
-require("./routes/HTMLroutes")(app)
+// app.use(express.static("public"));
 
+require("./routes/HTMLroutes")(app);
 
+app.get("/api/Close2You", function(req, res) {
+  res.sendFile(path.join(__dirname, "./Close2You.png"));
+});
 
-app.listen(PORT, function () {
-    console.log('Server listening on: http://localhost:' + PORT);
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
